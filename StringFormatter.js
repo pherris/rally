da29a6818c9651428796c678cdbf78dca8fd71bb
Andrew Ralston <aralston@andrews-mbp.gateway.2wire.net>
2014-02-22 21:37:09 -0800

    tweak to api and validation of template's successful replacement of all values

--- StringFormatter.js
+++ StringFormatter.js
@@ -1,13 +1,16 @@
 //TODO keys can have no spaces, but right no values cannot either.
 //is this the right api for multiple apps running on the same screen?
 //what do to if this namespace is already taken?
+//what if the format of the key changes?
+//dependencies on console.log and JSON.stringify
 var StringFormatter = (function () {
   'use strict';
   //this object takes a map of key/value pairs to be used in string formatting, then reuses that map for subsequent requests to the format engine.
   var formatValidator = new RegExp('^([\\w]+=[\\w]+,)?([\\w]+=[\\w]+){1}$'), //regex for map input validation
-      templateMatcher = new RegExp('\\${\\w+}'),
+      templateMatcher = new RegExp('\\${\\w+}', 'g'), //global flag is important due to how the format method uses it
       substitutionValues = {},  //object for easy lookup
-      template = '';
+      // template = '',
+      debug = false;
   
   return {
     /**
@@ -51,34 +54,53 @@ var StringFormatter = (function () {
       return substitutionValues;
     },
 
-    /**
-     * templateString any string with valid ${keys} in it. Keys can be escaped as in ${${key}}. Keys are expected to be any alphanumeric character from the basic Latin alphabet, including the underscore.
-     **/
-    setTemplate: function (templateString) {
-      //no validation really required...
-      template = templateString;
-    },
+    // /**
+    //  * templateString any string with valid ${keys} in it. Keys can be escaped as in ${${key}}. Keys are expected to be any alphanumeric character from the basic Latin alphabet, including the underscore.
+    //  **/
+    // setTemplate: function (templateString) {
+    //   //no validation really required...
+    //   template = templateString;
+    // },
 
-    /**
-     * gets the current value of the template string
-     **/
-    getTemplate : function () {
-      return template;
-    },
+    // /**
+    //  * gets the current value of the template string
+    //  **/
+    // getTemplate : function () {
+    //   return template;
+    // },
 
     /**
      * format takes optional parameters templateString and keyValuePairString for convenience.
      **/
     format : function (templateString, keyValuePairString) {
-      if (templateString) {
-        StringFormatter.setTemplate(templateString);
-      }
+      var formattedString = '',
+          matchedArray = [];
+
+      // if (templateString) {
+      //   StringFormatter.setTemplate(templateString);
+      // }
       
       if (keyValuePairString) {
         StringFormatter.setSubstitutionValues(keyValuePairString, true);
       }
-      
 
+      while ((matchedArray = templateMatcher.exec(templateString)) !== null) {
+        var keyName = matchedArray[0].substring(2, matchedArray[0].length - 1);
+        //found a templated var, do I have a config?
+        if (!substitutionValues[keyName]) {
+          throw { message: 'Found template key \'' + keyName + '\' with no value in template string: \'' + templateString + '\' with mapped values: \'' + JSON.stringify(substitutionValues) + '\'' };
+        }
+        var msg = "Found " + matchedArray[0] + ".  ";
+        msg += "Next match starts at " + templateMatcher.lastIndex;
+        console.log(msg);
+      }
+    },
+
+    /**
+     * enables some debug
+     **/
+    setDebug : function (isDebug) {
+      debug = isDebug;
     }
   }
 })();
