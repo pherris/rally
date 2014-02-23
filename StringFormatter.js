23cdf2db2614ada2973a73b58c99ca902ec1307a
Andrew Ralston <aralston@andrews-mbp.gateway.2wire.net>
2014-02-22 22:27:36 -0800

    refactoring some of the error handling to be less severe and fixed a bug with the regex

--- StringFormatter.js
+++ StringFormatter.js
@@ -1,13 +1,14 @@
 //TODO keys can have no spaces, but right no values cannot either.
 //is this the right api for multiple apps running on the same screen?
-//what do to if this namespace is already taken?
-//what if the format of the key changes?
 //dependencies on console.log and JSON.stringify
+if (StringFormatter) {
+  throw new Error ('Cannot create new StringFormatter singleton, an object already exists within that namespace. Did the library get importated more than once?');
+}
+
 var StringFormatter = (function () {
   'use strict';
   //this object takes a map of key/value pairs to be used in string formatting, then reuses that map for subsequent requests to the format engine.
   var formatValidator = new RegExp('^([\\w]+=[\\w]+,)?([\\w]+=[\\w]+){1}$'), //regex for map input validation
-      templateMatcher = new RegExp('\\${\\w+}', 'g'), //global flag is important due to how the format method uses it
       substitutionValues = {},  //object for easy lookup
       // template = '',
       debug = false,
@@ -31,12 +32,12 @@ var StringFormatter = (function () {
       //expected type is string
       if (typeof keyValuePairString !== "string") {
         log('type of key value pair is not string: ' + keyValuePairString);
-        throw { message: "Format of input expected to be string in StringFormatter.setSubstitutionValues()." };
+        throw new Error('Format of input expected to be string in StringFormatter.setSubstitutionValues().');
       }
       //expected format is key=value,key=value (not tolerant of spaces - yet ;)
       if (!formatValidator.exec(keyValuePairString)) {
         log('key value pair does not pass regex: ' + keyValuePairString);
-        throw { message: "String format invalid. Be sure input string matches \"key=value,key=value\" with no spaces. Expects any alphanumeric character from the basic Latin alphabet, including the underscore." };
+        throw new Error('String format invalid. Be sure input string matches \"key=value,key=value\" with no spaces. Expects any alphanumeric character from the basic Latin alphabet, including the underscore.');
       }
 
       log('key value pair passed validation');
@@ -67,46 +68,40 @@ var StringFormatter = (function () {
       return substitutionValues;
     },
 
-    // /**
-    //  * templateString any string with valid ${keys} in it. Keys can be escaped as in ${${key}}. Keys are expected to be any alphanumeric character from the basic Latin alphabet, including the underscore.
-    //  **/
-    // setTemplate: function (templateString) {
-    //   //no validation really required...
-    //   template = templateString;
-    // },
-
-    // /**
-    //  * gets the current value of the template string
-    //  **/
-    // getTemplate : function () {
-    //   return template;
-    // },
-
     /**
      * format takes optional parameters templateString and keyValuePairString for convenience.
      **/
     format : function (templateString, keyValuePairString) {
       var formattedString = '',
-          matchedArray = [];
-
+          matchedArray = [],
+          templateMatcher = new RegExp('\\${\\w+}', 'g'); //global flag is important due to how the format method uses it
+      
       // if (templateString) {
       //   StringFormatter.setTemplate(templateString);
       // }
       
       if (keyValuePairString) {
         log('using convenience method, resetting key value pairs.');
-        StringFormatter.setSubstitutionValues(keyValuePairString, true);
+        try {
+          StringFormatter.setSubstitutionValues(keyValuePairString, true);
+        } catch (e) {
+          log('failed to set new key/values, defaulting to existing values.')
+          console.log(e.message);
+        }
       }
 
+      log('start matching');
       while ((matchedArray = templateMatcher.exec(templateString)) !== null) {
         var keyName = matchedArray[0].substring(2, matchedArray[0].length - 1);
         //found a templated var, do I have a config?
         if (!substitutionValues[keyName]) {
           log('found a template key that had no mapped value. This is not allowed per the requirements (although, I\'d rather not throw a fatal error like this).');
-          throw { message: 'Found template key \'' + keyName + '\' with no value in template string: \'' + templateString + '\' with mapped values: \'' + JSON.stringify(substitutionValues) + '\'' };
+          //throw new Error('Found template key \'' + keyName + '\' with no value in template string: \'' + templateString + '\' with mapped values: \'' + JSON.stringify(substitutionValues) + '\'' );
+          console.log('Found template key \'' + keyName + '\' with no value in template string: \'' + templateString + '\' with mapped values: \'' + JSON.stringify(substitutionValues) + '\'');
+        } else {
+          log('found key: ' + matchedArray[0] + ', replacing with: ' + substitutionValues[keyName]);
+          templateString = templateString.replace(matchedArray[0], substitutionValues[keyName]);
         }
-        
-        templateString = templateString.replace(matchedArray[0], substitutionValues[keyName]);
       }
 
       return templateString;
